8b26249110b195a7dfe47edc58b98d7b6d6a3920
Илья Ершов <ershov.1ly4@gmail.com>
2022-07-05 16:24:35 +0600

    Added CommunicationServiceFactoryTests

---
+++ TestsDelivery.BL.UnitTests/Factories/CommunicationServiceFactoryTests.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Moq;
+using TestsDelivery.BL.Factories;
+using TestsDelivery.BL.Services.Communication;
+using TestsDelivery.BL.Shared.Clients.Integration;
+using Xunit;
+
+namespace TestsDelivery.BL.UnitTests.Factories
+{
+    public class CommunicationServiceFactoryTests
+    {
+        private IMapper _mapper;
+        private Mock<IIntegrationApiClient> _apiClientMock;
+        private CommunicationServiceFactory _factory;
+
+        public CommunicationServiceFactoryTests()
+        {
+            _mapper = new Mapper(new MapperConfiguration(cfg => { }));
+            _apiClientMock = new Mock<IIntegrationApiClient>();
+            _factory = new CommunicationServiceFactory(_mapper);
+        }
+
+        [Fact]
+        public void Create_TypeProvided_InstanceCreated()
+        {
+            // Arrange
+            const string instanceUrl = "https://www.example.com";
+
+            // Act
+            var createdService = _factory.Create<IAdminPanelCommunicationService>(_apiClientMock.Object, instanceUrl);
+
+            // Assert
+            Assert.IsType<AdminPanelCommunicationService>(createdService);
+        }
+    }
+}
